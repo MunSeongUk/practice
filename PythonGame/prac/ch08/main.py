d10c66ea816038b28368709390bdceab29697bdc
pocketmun1216 <pocketmun1216@gmail.com>
2023-04-03 02:09:18 +0900

    ch09 완료

--- PythonGame/prac/ch08/main.py
+++ PythonGame/prac/ch08/main.py
@@ -1,79 +1,227 @@
 import tkinter
-import tkinter.messagebox
+import random
 
-key = ""
+index = 0
+timer = 0
+score = 0
+hisc = 1000
+difficulty = 0
+tsugi = 0
 
-
-def key_down(e):
-    global key
-    key = e.keysym
-
-
-def key_up(e):
-    global key
-    key = ""
-
-
-mx = 1
-my = 1
-yuka = 0
+cursor_x = 0
+cursor_y = 0
+mouse_x = 0
+mouse_y = 0
+mouse_c = 0
 
 
-def main_proc():
-    global mx, my, yuka
-    if key == "Shift_L" and yuka > 1:
-        canvas.delete("PAINT")
-        mx = 1
-        my = 1
-        yuka = 0
-        for y in range(7):
-            for x in range(10):
-                if maze[y][x] == 2:
-                    maze[y][x] = 0
-    if key == "Up" and maze[my - 1][mx] == 0:
-        my = my - 1
-    if key == "Down" and maze[my + 1][mx] == 0:
-        my = my + 1
-    if key == "Left" and maze[my][mx - 1] == 0:
-        mx = mx - 1
-    if key == "Right" and maze[my][mx + 1] == 0:
-        mx = mx + 1
-    if maze[my][mx] == 0:
-        maze[my][mx] = 2
-        yuka = yuka + 1
-        canvas.create_rectangle(mx * 80, my * 80, mx * 80 + 79, my * 80 + 79, fill="pink", width=0, tag="PAINT")
-    canvas.delete("MYCHR")
-    canvas.create_image(mx * 80 + 40, my * 80 + 40, image=img, tag="MYCHR")
-    if yuka == 30:
-        canvas.update()
-        tkinter.messagebox.showinfo("축하합니다!", "모든 바닥을 칠했습니다!")
-    else:
-        root.after(300, main_proc)
+def mouse_move(e):
+    global mouse_x, mouse_y
+    mouse_x = e.x
+    mouse_y = e.y
+
+
+def mouse_press(e):
+    global mouse_c
+    mouse_c = 1
+
+
+neko = []
+check = []
+for idx in range(10):
+    neko.append([0, 0, 0, 0, 0, 0, 0, 0])
+    check.append([0, 0, 0, 0, 0, 0, 0, 0])
+
+
+def draw_neko():
+    cvs.delete("NEKO")
+    for y in range(10):
+        for x in range(8):
+            if neko[y][x] > 0:
+                cvs.create_image(x * 72 + 60, y * 72 + 60, image=img_neko[neko[y][x]], tag="NEKO")
+
+
+def check_neko():
+    for y in range(10):
+        for x in range(8):
+            check[y][x] = neko[y][x]
+
+    for y in range(1, 9):
+        for x in range(8):
+            if check[y][x] > 0:
+                if check[y - 1][x] == check[y][x] and check[y + 1][x] == check[y][x]:
+                    neko[y - 1][x] = 7
+                    neko[y][x] = 7
+                    neko[y + 1][x] = 7
+
+    for y in range(10):
+        for x in range(1, 7):
+            if check[y][x] > 0:
+                if check[y][x - 1] == check[y][x] and check[y][x + 1] == check[y][x]:
+                    neko[y][x - 1] = 7
+                    neko[y][x] = 7
+                    neko[y][x + 1] = 7
+
+    for y in range(1, 9):
+        for x in range(1, 7):
+            if check[y][x] > 0:
+                if check[y - 1][x - 1] == check[y][x] and check[y + 1][x + 1] == check[y][x]:
+                    neko[y - 1][x - 1] = 7
+                    neko[y][x] = 7
+                    neko[y + 1][x + 1] = 7
+                if check[y + 1][x - 1] == check[y][x] and check[y - 1][x + 1] == check[y][x]:
+                    neko[y + 1][x - 1] = 7
+                    neko[y][x] = 7
+                    neko[y - 1][x + 1] = 7
+
+
+def sweep_neko():
+    num = 0
+    for y in range(10):
+        for x in range(8):
+            if neko[y][x] == 7:
+                neko[y][x] = 0
+                num = num + 1
+    return num
+
+
+def drop_neko():
+    flg = False
+    for y in range(8, -1, -1):
+        for x in range(8):
+            if neko[y][x] != 0 and neko[y + 1][x] == 0:
+                neko[y + 1][x] = neko[y][x]
+                neko[y][x] = 0
+                flg = True
+    return flg
+
+
+def over_neko():
+    for x in range(8):
+        if neko[0][x] > 0:
+            return True
+    return False
+
+
+def set_neko():
+    for x in range(8):
+        neko[0][x] = random.randint(0, difficulty)
+
+
+def draw_txt(txt, x, y, siz, col, tg):
+    fnt = ("Times New Roman", siz, "bold")
+    cvs.create_text(x + 2, y + 2, text=txt, fill="black", font=fnt, tag=tg)
+    cvs.create_text(x, y, text=txt, fill=col, font=fnt, tag=tg)
+
+
+def game_main():
+    global index, timer, score, hisc, difficulty, tsugi
+    global cursor_x, cursor_y, mouse_c
+    if index == 0:
+        draw_txt("야옹야옹", 312, 240, 100, "violet", "TITLE")
+        cvs.create_rectangle(168, 384, 456, 456, fill="skyblue", width=0, tag="TITLE")
+        draw_txt("Easy", 312, 420, 40, "white", "TITLE")
+        cvs.create_rectangle(168, 528, 456, 600, fill="lightgreen", width=0, tag="TITLE")
+        draw_txt("Normal", 312, 564, 40, "white", "TITLE")
+        cvs.create_rectangle(168, 672, 456, 744, fill="orange", width=0, tag="TITLE")
+        draw_txt("Hard", 312, 708, 40, "white", "TITLE")
+        index = 1
+        mouse_c = 0
+    elif index == 1:
+        difficulty = 0
+        if mouse_c == 1:
+            if 168 < mouse_x < 456 and 384 < mouse_y < 456:
+                difficulty = 4
+            if 168 < mouse_x < 456 and 528 < mouse_y < 600:
+                difficulty = 5
+            if 168 < mouse_x < 456 and 672 < mouse_y < 744:
+                difficulty = 6
+            if difficulty > 0:
+                for y in range(10):
+                    for x in range(8):
+                        neko[y][x] = 0
+                mouse_c = 0
+                score = 0
+                tsugi = 0
+                cursor_x = 0
+                cursor_y = 0
+                set_neko()
+                draw_neko()
+                cvs.delete("TITLE")
+                index = 2
+    elif index == 2:
+        if not drop_neko():
+            index = 3
+        draw_neko()
+    elif index == 3:
+        check_neko()
+        draw_neko()
+        index = 4
+    elif index == 4:
+        sc = sweep_neko()
+        score = score + sc * difficulty * 2
+        if score > hisc:
+            hisc = score
+        if sc > 0:
+            index = 2
+        else:
+            if not over_neko():
+                tsugi = random.randint(1, difficulty)
+                index = 5
+            else:
+                index = 6
+                timer = 0
+        draw_neko()
+    elif index == 5:
+        if 24 <= mouse_x < 24 + 72 * 8 and 24 <= mouse_y < 24 + 72 * 10:
+            cursor_x = int((mouse_x - 24) / 72)
+            cursor_y = int((mouse_y - 24) / 72)
+            if mouse_c == 1:
+                mouse_c = 0
+                set_neko()
+                neko[cursor_y][cursor_x] = tsugi
+                tsugi = 0
+                index = 2
+        cvs.delete("CURSOR")
+        cvs.create_image(cursor_x * 72 + 60, cursor_y * 72 + 60, image=cursor, tag="CURSOR")
+        draw_neko()
+    elif index == 6:
+        timer = timer + 1
+        if timer == 1:
+            draw_txt("GAME OVER", 312, 348, 60, "red", "OVER")
+        if timer == 50:
+            cvs.delete("OVER")
+            index = 0
+    cvs.delete("INFO")
+    draw_txt("SCORE" + str(score), 160, 60, 32, "blue", "INFO")
+    draw_txt("HISC " + str(hisc), 450, 60, 32, "yellow", "INFO")
+    if tsugi > 0:
+        cvs.create_image(752, 128, image=img_neko[tsugi], tag="INFO")
+    root.after(100, game_main)
 
 
 root = tkinter.Tk()
-root.title("바닥을 칠한다냥")
-root.bind("<KeyPress>", key_down)
-root.bind("<KeyRelease>", key_up)
-canvas = tkinter.Canvas(width=800, height=560, bg="white")
-canvas.pack()
+root.title("낙하 퍼즐 '야옹야옹'")
+root.resizable(False, False)
+root.bind("<Motion>", mouse_move)
+root.bind("<ButtonPress>", mouse_press)
+cvs = tkinter.Canvas(root, width=912, height=768)
+cvs.pack()
 
-maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+bg = tkinter.PhotoImage(file="neko_bg.png")
+cursor = tkinter.PhotoImage(file="neko_cursor.png")
+
+img_neko = [
+    None,
+    tkinter.PhotoImage(file="neko1.png"),
+    tkinter.PhotoImage(file="neko2.png"),
+    tkinter.PhotoImage(file="neko3.png"),
+    tkinter.PhotoImage(file="neko4.png"),
+    tkinter.PhotoImage(file="neko5.png"),
+    tkinter.PhotoImage(file="neko6.png"),
+    tkinter.PhotoImage(file="neko_niku.png")
 ]
 
-for y in range(7):
-    for x in range(10):
-        if maze[y][x] == 1:
-            canvas.create_rectangle(x * 80, y * 80, x * 80 + 79, y * 80 + 79, fill="skyblue", width=0)
-
-img = tkinter.PhotoImage(file="mimi_s.png")
-canvas.create_image(mx * 80 + 40, my * 80 + 40, image=img, tag="MYCHR")
-main_proc()
+cvs.create_image(456, 384, image=bg)
+game_main()
 root.mainloop()
